46c6b87e3ca8e8a6f48038160a5283a0b9fd287d
Aleksandar Toslev <39882480+alexToslev@users.noreply.github.com>
2024-05-13 17:04:09 +0300

    Add files via upload

---
+++ Basics-of-the-traffic-system/PKW.h
@@ -0,0 +1,34 @@
+
+#ifndef PKW_H_
+#define PKW_H_
+
+#include "Fahrzeug.h"
+#include<limits>
+
+class PKW: public Fahrzeug
+{
+private:
+	double p_dVerbrauch = 0;	//Liter/100km
+	double p_dTankvolumen = 55;	//Liters
+	double p_dTankinhalt = double(p_dTankvolumen/2);
+
+public:
+
+	PKW();
+	PKW(string sName);
+	PKW(string sName, double dMaxGeschwindigkeit);
+	PKW(string sName, double dMaxGeschwindigkeit, double dVerbrauch, double dTankvolumen);
+
+	virtual void vAusgeben() override;
+	virtual double dTanken(double dMenge = numeric_limits<double>::infinity()) override;	// = numeric_limits<double>::infinity()
+	virtual void vSimulieren() override;
+
+	virtual void vAusgeben(ostream& os) const override;
+
+
+	virtual ~PKW();
+};
+
+
+
+#endif /* PKW_H_ */
