572706ea3091ba4b1afd34356374ebe9c4d03cc5
Aleksandar Toslev <39882480+alexToslev@users.noreply.github.com>
2024-05-15 19:03:27 +0300

    Add files via upload

---
+++ Basics-of-the-traffic-system/Fahrrad.h
@@ -0,0 +1,26 @@
+
+#ifndef FAHRRAD_H_
+#define FAHRRAD_H_
+
+#include "Fahrzeug.h"
+
+class Fahrrad: public Fahrzeug
+{
+public:
+	Fahrrad();
+	Fahrrad(string name, double maxGeschwindigkeit);
+
+	virtual double dGeschwindigkeit() const override;
+	//virtual void vAusgeben() override;
+	//virtual void vSimulieren() override;
+	//virtual void vAusgeben(ostream& os) override;
+
+
+
+	virtual ~Fahrrad();
+
+};
+
+
+
+#endif /* FAHRRAD_H_ */
