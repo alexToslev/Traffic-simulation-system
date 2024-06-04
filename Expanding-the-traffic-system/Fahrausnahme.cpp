9da145207a7f5165c035aa4ba463e8d3c0911a10
Aleksandar Toslev <39882480+alexToslev@users.noreply.github.com>
2024-06-04 11:58:24 +0300

    Fahrausnahme cpp

---
+++ Expanding-the-traffic-system/Fahrausnahme.cpp
@@ -0,0 +1,18 @@
+#include "Fahrausnahme.h"
+
+Fahrausnahme::Fahrausnahme(Fahrzeug& fahrzeug1, Weg& weg1) : p_pFahrzeugExc(fahrzeug1), p_pWegExc(weg1)
+{
+}
+
+Fahrausnahme::~Fahrausnahme()
+{
+}
+
+void Fahrausnahme::vBearbeiten()
+{
+
+
+}
+
+
+
