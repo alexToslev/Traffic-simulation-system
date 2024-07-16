d70b5f024113497b8d7e54ad12d4d50d29fa4f21
Aleksandar Toslev <39882480+alexToslev@users.noreply.github.com>
2024-07-16 09:26:52 +0200

    Tempolimit

---
+++ Expanding-the-traffic-system/Tempolimit.h
@@ -0,0 +1,15 @@
+
+#ifndef SRC_TEMPOLIMIT_H_
+#define SRC_TEMPOLIMIT_H_
+
+#include<limits>
+using namespace std;
+
+enum Tempolimit
+{
+	Innerorts = 50, Landstrasse = 100, Autobahn = numeric_limits<int>::max()
+
+};
+
+
+#endif
